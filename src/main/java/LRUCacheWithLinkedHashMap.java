732d8cb229b7cea8763d357c107f6033c7b4399e
Deep Kiran <deepkenzkiran@gmail.com>
2023-01-31 18:48:40 +0530

    negative edge cases are your best friends

--- src/main/java/LRUCacheWithLinkedHashMap.java
+++ src/main/java/LRUCacheWithLinkedHashMap.java
@@ -15,20 +15,17 @@ public class LRUCacheWithLinkedHashMap {
     }
    
     public int get(int key) {
-         if(map.containsKey(key))
-            {
+         if(map.containsKey(key)) {
                 int value=map.get(key);
                 map.remove(key);
                 map.put(key,value);
                 return value;
             }
-         else
-            return -1;
+         return -1;
     }
    
     public void set(int key, int value) {
-        if(map.containsKey(key))
-        {
+        if(map.containsKey(key)) {
             map.remove(key);
             map.put(key,value);
 
